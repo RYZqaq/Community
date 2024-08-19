5fd57e0a109894986d40297866ea41d3a7759c52
RYZqaq <453526241@qq.com>
2024-08-19 14:13:17 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -0,0 +1,128 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.service.AlphaService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+@Controller
+@RequestMapping("/alpha")
+public class AlphaController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello Spring Boot!";
+    }
+
+    @Autowired
+    private AlphaService alphaService;
+
+    @RequestMapping("/data")
+    @ResponseBody
+    public String data() {
+        return alphaService.find();
+    }
+
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response) {
+        //处理请求数据
+        System.out.println(request.getMethod());
+        System.out.println(request.getServletPath());
+
+        Enumeration<String> enumeration = request.getHeaderNames();
+
+        while(enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + ":" + value);
+        }
+
+        System.out.println(request.getParameter("code"));
+        //处理响应数据
+        response.setContentType("text/html;charset=utf-8");
+        try(PrintWriter writer = response.getWriter()) {
+            writer.write("<h1>牛客网</h1>");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Get 请求
+    //通过param传参
+    @RequestMapping(path = "/students", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudents(
+            @RequestParam(name = "current", required = false, defaultValue = "1") int current,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit
+    ) {
+        System.out.println(current);
+        System.out.println(limit);
+        return "student";
+    }
+
+    //通过路径传参
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudent(@PathVariable("id") int id) {
+        return "student" + id;
+    }
+
+    //Post请求
+    @RequestMapping(path = "/student", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name,int age, int id) {
+        System.out.println("学生:" + name + "学号:" + id + "年龄:" + age);
+        return "success";
+    }
+
+    //响应HTML数据
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTeather() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name","张三");
+        modelAndView.addObject("age","24");
+        modelAndView.setViewName("/demo/teacher");
+        return modelAndView;
+    }
+
+    @RequestMapping(path = "/school", method = RequestMethod.GET)
+    public String getSchool(Model model) {
+        model.addAttribute("name","北京大学");
+        return "/demo/school";
+    }
+
+    //响应JSON数据
+    @RequestMapping(path = "/emp", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Map<String,Object>> getEmp() {
+        List<Map<String,Object>> list = new ArrayList<>();
+
+        Map<String,Object> emp = new HashMap<>();
+        emp.put("name","张三");
+        emp.put("age",22);
+        emp.put("salary",8000.00);
+        list.add(emp);
+
+        emp = new HashMap<>();
+        emp.put("name","李四");
+        emp.put("age",23);
+        emp.put("salary",8000.00);
+        list.add(emp);
+
+        emp = new HashMap<>();
+        emp.put("name","王五");
+        emp.put("age",22);
+        emp.put("salary",8000.00);
+        list.add(emp);
+
+        return list;
+    }
+}
---
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -0,0 +1,48 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/index" ,method = RequestMethod.GET)
+    public String getIndexPage(Model model, Page page) {
+        //SpringMVC可以自动实例化参数（Model和Page），并且将其注入Model中，因此不需要addAttribute操作也可以直接在thymeleaf中访问Page
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
+        if(list != null) {
+            for(DiscussPost post : list) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("post", post);
+                User user = userService.findUserById(post.getUserId());
+                map.put("user", user);
+                discussPosts.add(map);
+            }
+        }
+        model.addAttribute("discussPosts", discussPosts);
+        return "index";
+    }
+}
