fd8dbd4e43ecf53e60813e275fce006cb5f9dcbd
RYZqaq <453526241@qq.com>
2024-08-20 11:48:48 +0800

    第二次提交

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,12 @@
+package com.nowcoder.community.util;
+
+public interface CommunityConstant {
+
+    int ACTIVATION_SUCCESS = 0; //激活成功
+
+    int ACTIVATION_REPEAT = 1; //重复激活
+
+    int ACTIVATION_FAILURE = 2; //激活失败
+
+
+}
